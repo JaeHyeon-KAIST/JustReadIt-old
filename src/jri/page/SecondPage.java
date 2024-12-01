84a6c04876bd2a4eca38c1324849a5294f76a8ba
JunginPark <pji0310@kaist.ac.kr>
2024-12-01 13:22:02 +0900

    Edit Second Page

--- src/jri/page/SecondPage.java
+++ src/jri/page/SecondPage.java
@@ -4,22 +4,30 @@ import jri.JRIApp;
 import jri.scenario.SecondScenario;
 import x.XPage;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JButton;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class SecondPage extends XPage {
   public static final String PAGE_NAME = "SecondPage";
-  private JLabel label;
   private JPanel secondPageGUI;
-  private JButton returnButton;
+  private JButton goToHomeButton;
+  private JList menuList;
+  private JTabbedPane noteListPannel;
+  private JLabel menu;
+  private JLabel bookName;
+  private JLabel bookAuthor;
+  private JLabel bookPublisher;
+  private JLabel bookPublicationYear;
+  private JLabel bookImage;
 
   public SecondPage(JRIApp app) {
     super(PAGE_NAME);
 
-    returnButton.addActionListener(new ActionListener() {
+    goToHomeButton.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
         SecondScenario scenario = (SecondScenario) app.getScenarioMgr().getCurScene().getScenario();
@@ -31,5 +39,28 @@ public class SecondPage extends XPage {
   @Override
   public JPanel getGUI() {
     return secondPageGUI;
+  }
+
+  private void createUIComponents() {
+    secondPageGUI = new JPanel();
+    secondPageGUI.setLayout(new BorderLayout());
+
+    // Get the current size of the frame (secondPageGUI)
+    int frameWidth = secondPageGUI.getWidth();
+
+    menu = new JLabel("Menu");
+    menuList = new JList();
+
+    goToHomeButton = new JButton("Go to Home");
+
+    noteListPannel = new JTabbedPane();
+
+    // Book information
+    bookImage = new JLabel("Book Image");
+    bookName = new JLabel("Book Name");
+    bookAuthor = new JLabel("Book Author");
+    bookPublisher = new JLabel("Publisher");
+    bookPublicationYear = new JLabel("Publication Year");
+
   }
 }
